6d8cadde2dfeab062d27dd5979009bbc110be716
Joji Koike <joji.koike@gmail.com>
2017-12-12 12:40:56 +0900

    idをuuid化

--- core/models/base/mastarbase.py
+++ core/models/base/mastarbase.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 
 
@@ -5,6 +6,7 @@ class MasterBase(models.Model):
     """
     Base Model Class for Master
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     
